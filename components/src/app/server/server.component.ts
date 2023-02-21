c0643e4579a4e5c5319d7d168893c72c7ab956ae
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2023-02-21 10:30:50 -0300

    add

--- components/src/app/server/server.component.ts
+++ components/src/app/server/server.component.ts
@@ -7,35 +7,42 @@ import type { Character } from "src/Types/Character";
     templateUrl: "./server.component.html",
     styleUrls: ["./server.component.scss"]
 })
-export class ServerComponent implements OnInit  {
+export class ServerComponent implements OnInit {
     characters: Character[] = []
     allCharacters: Character[] = []
     filterValue = ""
+    highlightFamily = false
 
     ngOnInit(): void {
         fetch("https://rickandmortyapi.com/api/character")
-        .then(val => val.json())
-        .then(({ results: characters }) => {
-            this.characters = characters
-            this.allCharacters = characters
-        })
+            .then(val => val.json())
+            .then(({ results: characters }) => {
+                this.characters = characters
+                this.allCharacters = characters
+            })
     }
 
     reset() {
         this.characters = [...this.allCharacters]
     }
 
-    log(val: string) {
-        alert(val)
+    toggleHighlightFamily = (val: boolean) => {
+        this.highlightFamily = val
     }
 
     filter(e: SubmitEvent) {
         e.preventDefault();
 
-        if (this.characters.length && this.filterValue) {
-            this.characters = this.allCharacters.filter(({name}) => name.toLowerCase().startsWith(this.filterValue.toLowerCase()))
+        if (this.filterValue) {
+            this.characters = this.allCharacters.filter(({ name }) => name.toLowerCase().startsWith(this.filterValue.toLowerCase()))
         } else if (this.filterValue === "") {
             this.characters = [...this.allCharacters]
         }
     }
+
+    randomSort() {
+        this.characters = [...this.characters.sort(() => Math.random() > .5 ? 1 : -1)]
+    }
+
+    getBgColor = (char: Character) => this.highlightFamily && char.name.includes('Smith') ? 'purple' : '#11191f'
 }
